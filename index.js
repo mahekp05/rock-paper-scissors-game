16ba784fa492cfd026aaff2be46ede1008677b59
Mahek Patel <mahekapatel@outlook.com>
2023-07-10 18:30:25 -0400

    more editing made to all three files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     choices = ['rock' , 'paper' , 'scissors'];
-    index = Math.floor(Math.random()*(4-1)+1) - 1;
+    index = Math.floor(Math.random()*choices.length);
     return choices[index];
     
 };
@@ -11,39 +11,49 @@ function game(cSelection, uSelection){
         case 'rock':
             if (uSelection === 'rock'){
                 return "It's an tie!!!";
-            } else if (uSelection === 'papers'){
+                break;
+            } else if (uSelection === 'paper'){
                 return 'You win this round';
+                break;
             } else if (uSelection === 'scissors'){
                 return 'Computer wins this round.';
+                break;
             }
         case 'scissors':
             if (uSelection === 'scissors'){
                 return "It's an tie!!!";
+                break;
             } else if (uSelection === 'rock'){
                 return 'You win this round';
+                break;
             } else if (uSelection === 'paper'){
                 return 'Computer wins this round.';
+                break;
             }
         case 'paper':
             if (uSelection === 'paper'){
                 return "It's an tie!!!";
+                break;
             } else if (uSelection === 'scissors'){
                 return 'You win this round';
+                break;
             } else if (uSelection === 'rock'){
                 return 'Computer wins this round.';
+                break;
             }
         }
     }
 
-const userChoiceB = document.querySelector("userChoiceButton");
+const userChoiceButtons = document.querySelectorAll(".userChoiceButton");
 const resultShow = document.querySelector("#results");
 let computerChoice = getComputerChoice();
 let playerChoice;
 let result;
 
-userChoiceB.forEach(button => button.addEventListener("click", () => {
+userChoiceButtons.forEach(button => button.addEventListener("click", () => {
     playerChoice = button.textContent;
-    result = game(computerChoice,playerChoice);
+    result = game(computerChoice, playerChoice);
     resultShow.textContent = result;
 }));
 
+/*https://www.youtube.com/watch?v=n1_vHArDBRA*/
